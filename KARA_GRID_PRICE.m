65b45def2cc2699e749c0b5a8e0dd6a4bbfc468b
MALIK2k21 <94821299+MALIK2k21@users.noreply.github.com>
2025-06-11 17:11:46 +0200

    Update KARA_GRID_PRICE.m

--- KARA_GRID_PRICE.m
+++ KARA_GRID_PRICE.m
@@ -1,4 +1,4 @@
-function price_per_kWh = KARA_GRID_PRICE(verbrauch, jahreshoechstleistung,peakloadcost)
+function price_per_kWh = case1_GRID_PRICE(verbrauch, jahreshoechstleistung,peakloadcost)
 
 %
 price_per_kWh = 0.020;
